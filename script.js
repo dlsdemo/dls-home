52029de8f71610259b88f31357996234483c223f
dlsdemo <135023076+dlsdemo@users.noreply.github.com>
2023-06-24 10:02:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Fetch the Cloudflare Worker response and update the code snippet section
+fetch("/getcolo")
+  .then((response) => response.text())
+  .then((data) => {
+    const codeSection = document.getElementById("box-1-code");
+    codeSection.innerHTML = `<pre><code>${data}</code></pre>`;
+    // `<pre><code>$ curl -k -s https://workers.dlsdemo.com/ | grep -o '"colo": *"[^"]*"' <br><br>${data}<br><br></code></pre>`
+  })
+  .catch((error) => console.log(error));
