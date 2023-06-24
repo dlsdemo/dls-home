77cf2aaaae9c187b41707adb5513b45f31e904f9
dlsdemo <135023076+dlsdemo@users.noreply.github.com>
2023-06-24 10:14:43 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ fetch("/getcolo")
   .then((response) => response.text())
   .then((data) => {
     const codeSection = document.getElementById("box-1-code");
-    codeSection.innerHTML = `<pre><code>${data}</code></pre>`;
-    // `<pre><code>$ curl -k -s https://workers.dlsdemo.com/ | grep -o '"colo": *"[^"]*"' <br><br>${data}<br><br></code></pre>`
+    //codeSection.innerHTML = `<pre><code>${data}</code></pre>`;
+    codeSection.innerHTML = `<pre><code>$ curl -k -s https://workers.dlsdemo.com/ | grep -o '"colo": *"[^"]*"' <br><br>${data}<br><br></code></pre>`;
   })
   .catch((error) => console.log(error));
