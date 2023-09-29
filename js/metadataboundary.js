956cc2e94c9780a5790a0424df826d82b79fe6d0
DavidJKTofan <dtofan@learning.eoi.es>
2023-09-29 02:56:51 +0200

    Fixed :)

---
+++ js/metadataboundary.js
@@ -0,0 +1,68 @@
+// Function to fetch GraphQL JSON data
+async function fetchData() {
+    try {
+        const response = await fetch(
+            "https://graphql-httprequestsadaptivegroups.dls-demo.workers.dev/"
+        );
+        const data = await response.json();
+        return data.data.viewer.zones[0].httpRequestsAdaptiveGroups;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        return [];
+    }
+}
+// Function to extract data for the chart
+async function createChart() {
+    const data = await fetchData();
+    const labels = data.map((item) => item.dimensions.datetimeHour);
+    const edgeResponseBytes = data.map((item) => item.sum.edgeResponseBytes);
+    const visits = data.map((item) => item.sum.visits);
+    // Create a chart using Chart.js
+    const ctx = document.getElementById("httpChart").getContext("2d");
+    const httpChart = new Chart(ctx, {
+        type: "bar",
+        data: {
+            labels: labels,
+            datasets: [{
+                    label: "Edge Response Bytes",
+                    data: edgeResponseBytes,
+                    backgroundColor: "rgba(75, 192, 192, 0.2)",
+                    borderColor: "rgba(75, 192, 192, 1)",
+                    borderWidth: 1,
+                },
+                {
+                    label: "Visits",
+                    data: visits,
+                    backgroundColor: "rgba(255, 99, 132, 0.2)",
+                    borderColor: "rgba(255, 99, 132, 1)",
+                    borderWidth: 1,
+                },
+            ],
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                },
+            },
+        },
+    });
+}
+// Render the chart
+createChart();
+// Workers GraphQL dataset
+// Function to fetch GraphQL JSON data
+async function fetchGraphQLData() {
+    try {
+        const response = await fetch('https://graphql-workersinvocationsadaptive.dls-demo.workers.dev/');
+        const graphqlData = await response.json();
+        const graphqlOutputDiv = document.getElementById('workersChart');
+        // Convert the JSON data to a pretty-printed string
+        const formattedJSON = JSON.stringify(graphqlData, null, 2);
+        // Insert the formatted JSON into the div
+        graphqlOutputDiv.textContent = formattedJSON;
+    } catch (error) {
+        console.error('Error fetching GraphQL data:', error);
+    }
+}
+fetchGraphQLData();
---
+++ js/regionalservices.js
@@ -0,0 +1,66 @@
+const worker1Route = "/getcolo-workers"; // Regional Services set to Earth
+const worker2Route = "https://usworkers.dlsdemo.com/"; // Regional Services set to USA
+const worker3Route = "https://euworkers.dlsdemo.com/"; // Regional Services set to EU
+const worker4Route = "/getcolo-pages"; // Regional Services set to Earth
+const worker5Route = "https://us.dlsdemo.com/getcolo-pages"; // Regional Services set to USA
+const worker6Route = "https://eu.dlsdemo.com/getcolo-pages"; // Regional Services set to EU
+
+// WORKERS 1 == EARTH
+fetch(worker1Route)
+  .then((response) => response.text())
+  .then((data) => {
+    const codeSection = document.getElementById("box-1-code");
+    codeSection.innerHTML = `<pre><span codeblock>${data}</span></pre>`;
+    // codeSection.innerHTML = `<pre><code>$ curl -s https://headers.dlsdemo.com/ | grep -o '"colo": *"[^"]*"' <br><br>${data}<br><br></code></pre>`;
+  })
+  .catch((error) => console.log(error));
+
+// WORKERS 2 == USA
+fetch(worker2Route)
+  .then((response) => response.text())
+  .then((data) => {
+    const codeSection = document.getElementById("box-2-code");
+    codeSection.innerHTML = `<pre><span codeblock>${data}</span></pre>`;
+    // codeSection.innerHTML = `<pre><code>$ curl -s https://usworkers.dlsdemo.com/ | grep -o '"colo": *"[^"]*"' <br><br>${data}<br><br></code></pre>`;
+  })
+  .catch((error) => console.log(error));
+
+// WORKERS 3 == EU
+fetch(worker3Route)
+  .then((response) => response.text())
+  .then((data) => {
+    const codeSection = document.getElementById("box-3-code");
+    codeSection.innerHTML = `<pre><span codeblock>${data}</span></pre>`;
+    // codeSection.innerHTML = `<pre><code>$ curl -s https://euworkers.dlsdemo.com/ | grep -o '"colo": *"[^"]*"' <br><br>${data}<br><br></code></pre>`;
+  })
+  .catch((error) => console.log(error));
+
+// PAGES 4 == EARTH
+fetch(worker4Route)
+  .then((response) => response.text())
+  .then((data) => {
+    const codeSection = document.getElementById("box-4-code");
+    codeSection.innerHTML = `<pre><span codeblock>${data}</span></pre>`;
+    // codeSection.innerHTML = `<pre><code>$ curl -sI https://bootstrap.dlsdemo.com/ | grep -oE 'cf-ray: [^[:space:]]+' | grep -oE '[^-]+$' | awk '{print "\"colo\":\""$0"\""}'<br><br>${data}<br><br></code></pre>`;
+  })
+  .catch((error) => console.log(error));
+
+// PAGES 5 == USA
+fetch(worker5Route)
+  .then((response) => response.text())
+  .then((data) => {
+    const codeSection = document.getElementById("box-5-code");
+    codeSection.innerHTML = `<pre><span codeblock>${data}</span></pre>`;
+    // codeSection.innerHTML = `<pre><code>$ curl -sI https://us.dlsdemo.com/ | grep -oE 'cf-ray: [^[:space:]]+' | grep -oE '[^-]+$' | awk '{print "\"colo\":\""$0"\""}'<br><br>${data}<br><br></code></pre>`;
+  })
+  .catch((error) => console.log(error));
+
+// PAGES 6 == EU
+fetch(worker6Route)
+  .then((response) => response.text())
+  .then((data) => {
+    const codeSection = document.getElementById("box-6-code");
+    codeSection.innerHTML = `<pre><span codeblock>${data}</span></pre>`;
+    // codeSection.innerHTML = `<pre><code>$ curl -sI https://eu.dlsdemo.com/ | grep -oE 'cf-ray: [^[:space:]]+' | grep -oE '[^-]+$' | awk '{print "\"colo\":\""$0"\""}'<br><br>${data}<br><br></code></pre>`;
+  })
+  .catch((error) => console.log(error));
