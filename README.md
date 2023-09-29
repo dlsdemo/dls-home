f0c48e95fdfc2bc8ceb5318bf3b6e978c3668e5e
DavidJKTofan <dtofan@learning.eoi.es>
2023-09-29 03:04:33 +0200

    Formatted and added mobile responsiveness

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Live DEMO of Cloudflare Data Localization Suite (DLS).
 
 Specifically of Regional Services and Customer Metadata Boundary.
 
+Learn more: [Navigating the changing data localization landscape with Cloudflare’s Data Localization Suite](https://blog.cloudflare.com/dls-2022/).
+
+* * * *
+
 # Disclaimer
 
 Educational purposes only.
--- js/regionalservices.js
+++ js/regionalservices.js
@@ -7,60 +7,60 @@ const worker6Route = "https://eu.dlsdemo.com/getcolo-pages"; // Regional Service
 
 // WORKERS 1 == EARTH
 fetch(worker1Route)
-  .then((response) => response.text())
-  .then((data) => {
-    const codeSection = document.getElementById("box-1-code");
-    codeSection.innerHTML = `<pre><span codeblock>${data}</span></pre>`;
-    // codeSection.innerHTML = `<pre><code>$ curl -s https://headers.dlsdemo.com/ | grep -o '"colo": *"[^"]*"' <br><br>${data}<br><br></code></pre>`;
-  })
-  .catch((error) => console.log(error));
+    .then((response) => response.text())
+    .then((data) => {
+        const codeSection = document.getElementById("box-1-code");
+        codeSection.innerHTML = `<pre><span codeblock>${data}</span></pre>`;
+        // codeSection.innerHTML = `<pre><code>$ curl -s https://headers.dlsdemo.com/ | grep -o '"colo": *"[^"]*"' <br><br>${data}<br><br></code></pre>`;
+    })
+    .catch((error) => console.log(error));
 
 // WORKERS 2 == USA
 fetch(worker2Route)
-  .then((response) => response.text())
-  .then((data) => {
-    const codeSection = document.getElementById("box-2-code");
-    codeSection.innerHTML = `<pre><span codeblock>${data}</span></pre>`;
-    // codeSection.innerHTML = `<pre><code>$ curl -s https://usworkers.dlsdemo.com/ | grep -o '"colo": *"[^"]*"' <br><br>${data}<br><br></code></pre>`;
-  })
-  .catch((error) => console.log(error));
+    .then((response) => response.text())
+    .then((data) => {
+        const codeSection = document.getElementById("box-2-code");
+        codeSection.innerHTML = `<pre><span codeblock>${data}</span></pre>`;
+        // codeSection.innerHTML = `<pre><code>$ curl -s https://usworkers.dlsdemo.com/ | grep -o '"colo": *"[^"]*"' <br><br>${data}<br><br></code></pre>`;
+    })
+    .catch((error) => console.log(error));
 
 // WORKERS 3 == EU
 fetch(worker3Route)
-  .then((response) => response.text())
-  .then((data) => {
-    const codeSection = document.getElementById("box-3-code");
-    codeSection.innerHTML = `<pre><span codeblock>${data}</span></pre>`;
-    // codeSection.innerHTML = `<pre><code>$ curl -s https://euworkers.dlsdemo.com/ | grep -o '"colo": *"[^"]*"' <br><br>${data}<br><br></code></pre>`;
-  })
-  .catch((error) => console.log(error));
+    .then((response) => response.text())
+    .then((data) => {
+        const codeSection = document.getElementById("box-3-code");
+        codeSection.innerHTML = `<pre><span codeblock>${data}</span></pre>`;
+        // codeSection.innerHTML = `<pre><code>$ curl -s https://euworkers.dlsdemo.com/ | grep -o '"colo": *"[^"]*"' <br><br>${data}<br><br></code></pre>`;
+    })
+    .catch((error) => console.log(error));
 
 // PAGES 4 == EARTH
 fetch(worker4Route)
-  .then((response) => response.text())
-  .then((data) => {
-    const codeSection = document.getElementById("box-4-code");
-    codeSection.innerHTML = `<pre><span codeblock>${data}</span></pre>`;
-    // codeSection.innerHTML = `<pre><code>$ curl -sI https://bootstrap.dlsdemo.com/ | grep -oE 'cf-ray: [^[:space:]]+' | grep -oE '[^-]+$' | awk '{print "\"colo\":\""$0"\""}'<br><br>${data}<br><br></code></pre>`;
-  })
-  .catch((error) => console.log(error));
+    .then((response) => response.text())
+    .then((data) => {
+        const codeSection = document.getElementById("box-4-code");
+        codeSection.innerHTML = `<pre><span codeblock>${data}</span></pre>`;
+        // codeSection.innerHTML = `<pre><code>$ curl -sI https://bootstrap.dlsdemo.com/ | grep -oE 'cf-ray: [^[:space:]]+' | grep -oE '[^-]+$' | awk '{print "\"colo\":\""$0"\""}'<br><br>${data}<br><br></code></pre>`;
+    })
+    .catch((error) => console.log(error));
 
 // PAGES 5 == USA
 fetch(worker5Route)
-  .then((response) => response.text())
-  .then((data) => {
-    const codeSection = document.getElementById("box-5-code");
-    codeSection.innerHTML = `<pre><span codeblock>${data}</span></pre>`;
-    // codeSection.innerHTML = `<pre><code>$ curl -sI https://us.dlsdemo.com/ | grep -oE 'cf-ray: [^[:space:]]+' | grep -oE '[^-]+$' | awk '{print "\"colo\":\""$0"\""}'<br><br>${data}<br><br></code></pre>`;
-  })
-  .catch((error) => console.log(error));
+    .then((response) => response.text())
+    .then((data) => {
+        const codeSection = document.getElementById("box-5-code");
+        codeSection.innerHTML = `<pre><span codeblock>${data}</span></pre>`;
+        // codeSection.innerHTML = `<pre><code>$ curl -sI https://us.dlsdemo.com/ | grep -oE 'cf-ray: [^[:space:]]+' | grep -oE '[^-]+$' | awk '{print "\"colo\":\""$0"\""}'<br><br>${data}<br><br></code></pre>`;
+    })
+    .catch((error) => console.log(error));
 
 // PAGES 6 == EU
 fetch(worker6Route)
-  .then((response) => response.text())
-  .then((data) => {
-    const codeSection = document.getElementById("box-6-code");
-    codeSection.innerHTML = `<pre><span codeblock>${data}</span></pre>`;
-    // codeSection.innerHTML = `<pre><code>$ curl -sI https://eu.dlsdemo.com/ | grep -oE 'cf-ray: [^[:space:]]+' | grep -oE '[^-]+$' | awk '{print "\"colo\":\""$0"\""}'<br><br>${data}<br><br></code></pre>`;
-  })
-  .catch((error) => console.log(error));
+    .then((response) => response.text())
+    .then((data) => {
+        const codeSection = document.getElementById("box-6-code");
+        codeSection.innerHTML = `<pre><span codeblock>${data}</span></pre>`;
+        // codeSection.innerHTML = `<pre><code>$ curl -sI https://eu.dlsdemo.com/ | grep -oE 'cf-ray: [^[:space:]]+' | grep -oE '[^-]+$' | awk '{print "\"colo\":\""$0"\""}'<br><br>${data}<br><br></code></pre>`;
+    })
+    .catch((error) => console.log(error));
